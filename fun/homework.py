f629fddb81b99af5f81a7751e44c3a007ee68bc8
Stacy Kemunto <kemuntos@vcu.edu>
2021-03-09 19:39:11 -0500

    change

--- fun/homework.py
+++ fun/homework.py
@@ -6,6 +6,14 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
+    #magiclownumber= none
+    #retval= magiclownumber
+    #for  value in incoming_list:
+        #if not retval:
+            #retval = value
+    #   if value> retvale
+          #retval= value
+    #return retval
     greatest_number = max(incoming_list)
     return greatest_number
 
@@ -24,7 +32,11 @@ def add_list_numbers(incoming_list):
     Required parameter, incoming_list, should be a list.
     Add all the values together and return it.
     """
-    summation = sum(incoming_list)
+    # summation=0
+    if incoming_list:
+       summation = sum(incoming_list)
+    else:
+        summation = 0
     return summation
 
 
@@ -33,8 +45,16 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-
-    longest_key = len(max(incoming_dict, key=len))
-    return longest_key
-
+    if not incoming_dict:
+        return None
+    all_keys = incoming_dict.keys()
+    if not all_keys:
+        return None
+    longest_value=None
+    for key in all_keys:
+        if not longest_value:
+            longest_value= key
+        if len(incoming_dict(key))>len(incoming_dict[longest_value]):
+            longest_value=key
+    return longest_value
 
